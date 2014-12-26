6c7dd9387e119b1e3c7ac94332bc3c0c29636acd
hunter <347212291@qq.com>
2014-12-26 23:04:26 +0800

    add wechat

--- app.rb
+++ app.rb
@@ -1,7 +1,25 @@
 require 'sinatra'
+require 'active_support/core_ext/hash'
 require './helper'
 
-get "/" do
-  p secret("hshshie94", "93shis", "你好")
-  erb :text
-end
+  before do
+    timestamp, nonce = params[:timestamp].to_s, params[:nonce].to_s
+    codes = ["jwcbit", timestamp, nonce].sort.join("")
+    halt!('401 Unauthorized', 401) unless Digest::SHA1.hexdigest(codes) == params[:signature]
+    status(200)
+  end
+
+  get '/' do
+    params['echostr']
+  end
+
+  post '/' do
+    @params = Hash.from_xml(request.body.read)['xml']
+    case @params['MsgType']
+    when "text"
+      @params['Content'] = secret("#{params['FromUserName']}",
+                                  "#{@params['ToUserName']}",
+                                  "#{@params['Content']}")
+      erb :text
+    end
+  end
