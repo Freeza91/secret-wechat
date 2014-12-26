7fb5568fb40e38d652ebfe6723a89302f804908a
rudy <347212291@qq.com>
2014-12-26 14:33:41 +0800

    add secret encrypt

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
+require './helper'
 
 get "/" do
+  p secret("hshshie94", "93shis", "你好")
   erb :text
 end
---
+++ helper.rb
@@ -0,0 +1,42 @@
+require 'digest'
+require "base64"
+
+AUTHOR = "rudyboy"
+EMAIL = "useyes91@gmail.com"
+SCHOOL = "BIT"
+KEY = "xxxx"
+
+helpers do
+
+  def secret(user_id, my_id, user_content)
+    encode_content = Base64.encode64(user_content)
+    code = Digest::SHA2.hexdigest("#{encode_content}-#{user_id}-#{my_id}")
+    key = Digest::SHA256.base64digest("#{code}-#{AUTHOR}@#{SCHOOL}by#{EMAIL}use#{KEY}")
+
+    num = "#{user_id}"[-1].ord
+    left = num % 16
+    right = key.size
+
+    password = ""
+    left.upto(right) do |i|
+      password += key[i] if is_ok?(key[i])
+      return password if password.size >= 16
+    end
+  end
+
+  def is_ok?(chr)
+    isok = case chr
+    when '0'..'9'
+      true
+    when 'a'..'z'
+      true
+    when 'A'..'Z'
+      true
+    else
+      false
+    end
+    isok
+  end
+
+end
+
