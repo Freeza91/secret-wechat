0ece286faeb71aa34a219e8bfab4945d87d3d4b8
hunter <347212291@qq.com>
2014-12-27 11:01:12 +0800

    add comments

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'active_support/core_ext/hash'
 require './helper'
 
+  #对微信发来的所有消息进行验证
   before do
     timestamp, nonce = params[:timestamp].to_s, params[:nonce].to_s
     codes = ["jwcbit", timestamp, nonce].sort.join("")
@@ -9,17 +10,24 @@ require './helper'
     status(200)
   end
 
+  #验证成功
   get '/' do
     params['echostr']
   end
 
+  #处理post请求
   post '/' do
+
+    #读取参数
     @params = Hash.from_xml(request.body.read)['xml']
+
     case @params['MsgType']
     when "text"
-      @params['Content'] = secret("#{params['FromUserName']}",
+      #加密
+      @params['Content'] = secret("#{@params['FromUserName']}",
                                   "#{@params['ToUserName']}",
                                   "#{@params['Content']}")
+      #返回加密结果
       erb :text
     end
   end
--- helper.rb
+++ helper.rb
@@ -4,19 +4,25 @@ require "base64"
 AUTHOR = "rudyboy"
 EMAIL = "useyes91@gmail.com"
 SCHOOL = "BIT"
-KEY = "xxxx"
+KEY = "my_private_key"
 
 helpers do
 
   def secret(user_id, my_id, user_content)
+
+    #base64 编码，更好支持中文
     encode_content = Base64.encode64(user_content)
+
+    #加密算法
     code = Digest::SHA2.hexdigest("#{encode_content}-#{user_id}-#{my_id}use#{KEY}")
     key = Digest::SHA256.base64digest("#{code}-#{AUTHOR}@#{SCHOOL}by#{EMAIL}")
 
+    #提取最后一个字符的ascii
     num = "#{user_id}"[-1].ord
     left = num % 16
     right = key.size
 
+    #截取16个字符串
     password = ""
     left.upto(right) do |i|
       password += key[i] if is_ok?(key[i])
@@ -24,6 +30,7 @@ helpers do
     end
   end
 
+  #处理特殊字符
   def is_ok?(chr)
     isok = case chr
     when '0'..'9'
@@ -35,6 +42,7 @@ helpers do
     else
       false
     end
+
     isok
   end
 
